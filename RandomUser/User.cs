079958a4c79cf85277bb69499890b869159cc505
Ruslan Albu <ruslan.albu@kurtosys.com>
2014-09-18 17:29:23 +0100

    just new class

---
+++ RandomUser/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace RandomUser
+{
+    public class User
+    {
+        public void Random()
+        {
+            HttpClient client = new HttpClient();
+
+        }
+    }
+}
---
+++ RandomUserTest/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RandomUser;
+using Xunit;
+
+namespace RandomUserTest
+{
+    public class Class1
+    {
+
+        [Fact]
+        public void Abc()
+        {
+           Assert.True(true); 
+            User user = new User();
+            user.Random();
+
+//            User.Random(); implment this
+        }
+    }
+}
