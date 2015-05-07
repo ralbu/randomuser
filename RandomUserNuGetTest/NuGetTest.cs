e4011b1c0a03472391c8ee5ae566c68ed3b31807
Ruslan <ruslan.albu@hotmail.com>
2015-05-07 12:27:27 +0100

    nuget test added

---
+++ RandomUserNuGetTest/NuGetTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using RandomUser;
+using Xunit;
+
+namespace RandomUserNuGetTest
+{
+    public class NuGetTest
+    {
+        [Fact]
+        public async Task GetRandomUser_ShouldReturnUser()
+        {
+            var user = await RandomUserApi.GetUser();
+
+            Assert.NotNull(user);
+            Assert.True(user.Name.First.Length > 0);
+        }
+    }
+}
