1684d0198f25dd86565f7fa19c4a9b76f59e069c
ruslan <ruslan.albu@hotmail.com>
2019-10-15 23:07:50 +0100

    tests added

---
+++ RandomUserTest/IntegrationTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using RandomUser;
+using Xunit;
+
+namespace RandomUserTest
+{
+    public class IntegrationTest
+    {
+        [Fact] public async Task GetRandomUser_ShouldReturnUser()
+        {
+            var user = await RandomUserApi.GetUser();
+
+            Assert.NotNull(user);
+            Assert.True(user.Name.First.Length > 0);
+       }
+    }
+}
